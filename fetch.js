494a819090d3f2d5fa521e59912498d9543ff4ca
contactjual <contactjual@gmail.com>
2024-11-07 13:00:00 +0600

    second commit

--- fetch.js
+++ fetch.js
@@ -1,3 +1,23 @@
+// fetch er kaj hocche data anaa 
+
+
 fetch('https://jsonplaceholder.typicode.com/todos/1')
-.then(response => response.json())
-.then(json => console.log(json))
+    .then(response => response.json())
+    .then(json => console.log(json))
+
+
+function loadData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => displayUsers(data))
+}
+
+function displayUsers(data) {
+    const ul = document.getElementById('user-list');
+    for (const user of data) {
+        console.log(user.name);
+        const list = document.createElement('li');
+        list.innerText= user.name;
+        ul.appendChild(list);
+    }
+}
